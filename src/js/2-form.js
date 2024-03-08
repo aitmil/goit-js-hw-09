faaced685d8fd1acf2cd0248f16ef7710407a768
aitmil <120422761+aitmil@users.noreply.github.com>
2024-03-08 20:46:48 +0200

    Update form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -10,24 +10,21 @@ try {
   console.log('No saved data');
 }
 
+let formData = {};
+
 form.addEventListener('input', onInput);
-function onInput() {
-  const email = form.elements.email.value.trim();
-  const message = form.elements.message.value.trim();
-  const data = JSON.stringify({ email, message });
-  localStorage.setItem(localStorageKey, data);
+function onInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
 }
 
 form.addEventListener('submit', onSubmit);
 function onSubmit(e) {
   e.preventDefault();
-  const email = form.elements.email.value;
-  const message = form.elements.message.value;
-  if (email !== '' || message !== '') {
-    console.log({
-      email,
-      message,
-    });
+  const input = document.querySelector('input');
+  const textarea = document.querySelector('textarea');
+  if (input.value !== '' || textarea.value !== '') {
+    console.log(formData);
   }
   localStorage.removeItem(localStorageKey);
   form.reset();
